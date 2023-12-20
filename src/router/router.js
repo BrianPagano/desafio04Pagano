a1efdb4ded571b3e96515087effe3ac4bf85c4ce
Brian Pagano <rocksound72@gmail.com>
2023-12-19 21:25:38 -0300

    desafio 04, con front de products con boostrap

--- src/router/router.js
+++ src/router/router.js
@@ -4,6 +4,7 @@ const cartsController = require ('../controllers/carts.controller')
 const router = app => {
     app.use('/api/products', productsController)
     app.use('/api/carts', cartsController)
+    app.use('/api/realtimeproducts', productsController)
 }
 
 
